36fbb0dac779e1457b53423cc148935789e16ad0
SDAlex66 <alex@alextula.com>
2026-08-19 23:53:58 +0100

    Bigger layout, gauges (84px), consistent spacing, API 500 retry

--- source/templates/kitty-mocha/template.mjs
+++ source/templates/kitty-mocha/template.mjs
@@ -1,5 +1,8 @@
 /**Template processor */
-export default async function({q}, _, {imports}) {
+export default async function({q, plugins}, _, {imports}) {
   await imports.plugins.core(...arguments)
+  if (plugins.pagespeed && plugins.pagespeed.error) {
+    throw new Error(`PageSpeed API error`)
+  }
   q.raw = true
 }
